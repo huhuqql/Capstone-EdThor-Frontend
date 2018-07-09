da226ec4d2a139eca7cc5d7f5a3bc8f999586027
siyu <siyuchen19950111@gmail.com>
2018-07-09 10:18:14 +0800

    add solution steps with kc targetting

--- Capstone-EdThor/src/app/multiple-choice-solution-steps/multiple-choice-solution-steps.component.ts
+++ Capstone-EdThor/src/app/multiple-choice-solution-steps/multiple-choice-solution-steps.component.ts
@@ -16,4 +16,7 @@ export class MultipleChoiceSolutionStepsComponent {
 
   @Input() cur_problem: Problem;
   @Input() cur_step: number;
+
+  kc_names = ['任意角的弧度制和任意角的三角函数', '同角三角函数的基本关系式和诱导公式', '三角函数的图像与性质', '三角函数图像变换', '正弦定理', '余弦定理', '斜三角形面积公式'];
+
 }
