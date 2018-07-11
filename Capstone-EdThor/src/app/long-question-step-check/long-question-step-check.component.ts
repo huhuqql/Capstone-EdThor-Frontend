ef8b9ecabe28a52074c3dae7a26f986151791eec
siyu <siyuchen19950111@gmail.com>
2018-07-11 11:40:34 +0800

    fix bug

--- Capstone-EdThor/src/app/long-question-step-check/long-question-step-check.component.ts
+++ Capstone-EdThor/src/app/long-question-step-check/long-question-step-check.component.ts
@@ -23,7 +23,7 @@ export class LongQuestionStepCheckComponent implements OnInit {
   checked: boolean[] = [];
   cur_kcs: string[] = [];
   kc_names = ['任意角的弧度制和任意角的三角函数', '同角三角函数的基本关系式和诱导公式', '三角函数的图像与性质', '三角函数图像变换', '正弦定理', '余弦定理', '斜三角形面积公式'];
-  checked_alternate: boolean;
+  checked_alternate: boolean = false;
 
   ngOnInit() {
     for (var i = 0; i < this.cur_problem.problem_long_question_solution[this.cur_sub_prob].length; i++) {
